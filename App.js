beacc8512277c50124001ca39f83ebba3f9d5852
unknown <singhsusna2003002@gmail.com>
2024-08-06 10:42:14 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+var a = 10
+console.log(a)
+const heading = React.createElement("h1", {}, "Hello World from React!")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
