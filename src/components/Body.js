d9aff523a7e6f323386d1e689d4efe697ecdda93
unknown <singhsusna2003002@gmail.com>
2024-08-28 16:12:15 +0530

    episode-05-2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,40 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/MockData";
+import { useState } from "react";
+
 
 const Body = () => {
-    return (
-        <div className='body'>
-            <div className='filter'>
-                <button className="filter-btn" onClick={()=>{console.log("There are no good Restaurants")}} onMouseOver={()=>{console.log("Do you wanna find a Good Restaurant")}}>Top Rated Restaurants</button>
-            </div>
-            <div className='res-container'>
-                {
-                    resObj.map((restaurant) => (<RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>))
-                }
-            </div>
-        </div>
-    )
-}
+  // Local State Variable - useState
 
-export default Body
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resObj);
+
+  const handleClick = () => {
+    const resData = resObj.filter((res) => res.info.avgRating >= 4);
+
+    setlistOfRestaurants(resData)
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onMouseOver={() => {
+            console.log("Do you wanna find a Good Restaurant");
+          }}
+
+          onClick={handleClick}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
